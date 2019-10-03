012aad53d55acc955ed1aef6d2292b8cc0ff5d15
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-10-03 19:33:29 +0530

    Add report function servlet

---
+++ src/com/wms/servlet/GRNGDNReport.java
@@ -0,0 +1,48 @@
+package com.wms.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/GRNGDNReport")
+public class GRNGDNReport extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public GRNGDNReport() {
+        super();
+       
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String step = request.getParameter("step");
+		HttpSession httpSession = request.getSession();
+		
+		if(step.equals("1")) {		
+			
+			httpSession.setAttribute("GRNNo", request.getParameter("GRNNo"));
+			response.sendRedirect("views/GoodHandling/ReportGRN.jsp");
+			
+		}
+		
+		else if(step.equals("2")) {
+			
+			httpSession.setAttribute("GDNNo", request.getParameter("GDNNo"));
+			response.sendRedirect("views/GoodHandling/ReportGDN.jsp");
+		}
+	}
+
+}
